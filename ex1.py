c3045ab672b3006385e38da54b41514b8f91aada
affection123456 <affection123456@outlook.com>
2017-05-13 20:47:17 +0800

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf -*-
+# '# -*- coding: utf -*-' == 'encoding: utf-8'
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay!printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print u"晴天" 
+# 输出中文的时候要在字符串前面加u(在linux系统不需要加u)
+# 因为python中默认采用ASCII,要声明为unicode就要加u
