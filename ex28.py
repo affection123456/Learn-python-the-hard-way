8c24758c39e08e08b8318e96691cf8bda894aac6
affection123456 <affection123456@outlook.com>
2017-05-20 13:43:18 +0800

    Add files via upload

---
+++ ex28.py
@@ -0,0 +1,28 @@
+print True and True 
+print False and False 
+print 1 == 1 and 2 == 1 
+print "test" == "test"
+print 1 == 1 or 2 != 1
+print True and 1 == 1 
+print False and 0 != 0
+print True or 1 == 1
+print "test" == "testing"
+print 1 != 0 and 2 == 1
+print "test" != "testing"
+print "test" == 1
+print not (True and False)
+print not (1 == 1 and 0 != 1)
+print not (10 == 1 or 1000 == 1000 )
+print not (1 != 10 or 3 == 4)
+print not ("testing" == "testing" and "Zed" == "Cool Guy")
+print 1 == 1 and not ("tesing" == 1 or 1 == 0)
+print "chunky" == "bacon" and not (3 == 4 or 3 == 3)
+print 3 == 3 and not ("testing" == "testing" or "Python" == "Fun")
+
+# I think it is a bug.
+print False and "whatever it is" , "is the only result."
+print True or "whatever it is" , "is the only result."
+
+# have a try 
+print "a" and "b"  # return b
+print "a" or "b"  # return a
