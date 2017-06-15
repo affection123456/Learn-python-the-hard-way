289249fa057fe0d4ac68a58f76542ddbcb90a764
affection123456 <affection123456@outlook.com>
2017-06-15 11:30:58 +0800

    Add files via upload

---
+++ ex44a.py
@@ -0,0 +1,15 @@
+# Implicit Inheritance
+
+class Parent(object):
+
+    def implict(self):
+        print "PARENT implict()"
+
+class Child(Parent):
+    pass
+
+dad = Parent()
+son = Child()
+
+dad.implict()
+son.implict()
